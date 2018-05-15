bb54b3669ceafc2fb3c80d23b33e8a2af48212e0
IvanOfficial <gerasimov.iv@phystech.edu>
2018-05-15 18:21:56 +0300

    issues

--- no_headers/main.cpp
+++ no_headers/main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 
 int main() 
 {
-    SimpleTree<int> c;
+    Container<int>* c = new SimpleTree<int>();
 
     for(int i = 1; i < 10; i++)
     {
-        c.insert(i*i);
+        c->insert(i*i);
     }
 
     printf("List after creation:\n");
-    c.print();
+    c->print();
 
-    if(c.exists(25))
+    if(c->exists(25))
         cout << "Search for value 25: found" << endl;
 
-    if(!c.exists(111))
+    if(!c->exists(111))
         cout << "Search for value 111: not found" << endl;
 
-    c.remove(25);
+    c->remove(25);
     cout << "Tree after deletion of the element:" << endl;
-    c.print();
-
+    c->print();
+    delete c;
     return 0;
 }
